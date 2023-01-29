cdf3b589bfd660cf9785a534f2b1e8253e7db833
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-01-28 21:07:29 -0600

    Logic for employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,36 @@
 // Employee is parent class
+
+// const { default: test } = require("node:test")
+
+// require Employee class
+
+// write a test to see if you can initiate an Employee instance
+test("Can instantiate Employee instance", () => {
+    // variable for the new Employee
+    // expect the type of that variable to be an object
+});
+
+// write a test to see if you can set the name as a constructor argument
+test("Can set name via constructor arguments", () => {
+  // variable for name
+  // variable for a new Employee with that name passed in as an argument
+  // expect the variable.name to be the variable that you set up for name
+});
+
+// same as above for id
+
+// same as above for email
+
+// write a test to see if your getter method is working
+test("Can get name via getName()", () => {
+  // get Name method should return name of employee
+  // variable for the test value
+  // variable for the new Employee with that test value passed in
+  // expect that when you call that method - to be the test value var
+});
+
+// same for getting the id
+
+// same for getting the email
+
+// same for getting the role
